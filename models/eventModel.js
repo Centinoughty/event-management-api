d55bd194fc316ea4910cbbb89aa665cc6f8c1f96
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2024-12-11 03:02:18 +0530

    created event model

---
+++ models/eventModel.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const EventSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+    venue: {
+      type: String,
+      required: true,
+    },
+    capacity: {
+      type: Number,
+      required: 0,
+      min: 0,
+    },
+    organizer: {
+      type: String,
+      required: true,
+    },
+    tags: {
+      type: [String],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("events", EventSchema);
