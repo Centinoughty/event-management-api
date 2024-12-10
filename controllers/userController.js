cc1a7d9b1e8f17eb20563ed1714af388657a3122
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2024-12-11 05:20:44 +0530

    crt: login route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 const { generateId } = require("../util/generateId");
 
@@ -31,5 +32,29 @@ module.exports.registerUser = async (req, res) => {
     res.status(201).json({ message: "Account created" });
   } catch (error) {
     res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+module.exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Cannot find user" });
+    }
+
+    if (!(await user.isMatchPassword(password))) {
+      return res.status(401).json({ message: "Incorrect password" });
+    }
+
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+    res.status(200).json({ message: "Login success", token });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+    console.log(error);
   }
 };
