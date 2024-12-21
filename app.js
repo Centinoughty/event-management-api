c4f6c1af82a9257e0f5312eb8d13ed7797f1895d
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2024-12-21 18:23:26 +0530

    cerated session logs

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const path = require("path");
+const fs = require("fs");
 const { connectDb } = require("./config/db");
 require("dotenv").config();
 require("./tasks/cronTask");
@@ -8,11 +10,18 @@ const venueRoute = require("./routes/venue");
 const eventRoute = require("./routes/event");
 const attendanceRoute = require("./routes/attendance");
 const feedbackRoute = require("./routes/feedback");
+const logAccess = require("./middlewares/logAccess");
 
 const app = express();
 const PORT = process.env.PORT;
 
+const logDir = path.join(__dirname, "/logs");
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+
 app.use(express.json());
+app.use(logAccess);
 
 app.use("/api/user", userRoute);
 app.use("/api/venue", venueRoute);
